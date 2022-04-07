cfe22bbc7fcb6dd3676a22c515f8c36922b27a8d
qaderi <qadari.444@gmail.com>
2022-04-08 00:18:05 +0430

    helper module added

--- plopfile.js
+++ plopfile.js
@@ -1,4 +1,5 @@
 const { NETWORK, BACKEND_DIR_NAME, CONTRACT_NAME, FRONTEND_DIR_NAME } = require('./constant')
+const {toPascalCase} = require('./helper')
 module.exports = function (plop) {
     plop.setGenerator('init', {
         description: 'preparing shell scripts',
@@ -50,71 +51,64 @@ module.exports = function (plop) {
     plop.setGenerator('backend-generator', {
         description: 'installing and generating backend dependencies and file',
 
-        prompts: [
-            {
-                type: 'input',
-                name: 'contractName',
-                default: CONTRACT_NAME,
-                message: 'Press enter!, for contract name'
-            },
-            {
-                type: 'input',
-                name: 'network',
-                default: NETWORK,
-                message: 'Press Enter!, for network name'
-            }
-        ],
+        prompts: [],
 
         actions: [
             {
                 type: 'add',
                 path: `../${BACKEND_DIR_NAME}/.env`,
-                templateFile: 'plop-templates/backend/env.hbs'
+                templateFile: 'plop-templates/backend/env.hbs',
+                data:{
+                    network: NETWORK
+                }
             },
             {
                 type: 'add',
                 path: `../${BACKEND_DIR_NAME}/hardhat.config.js`,
-                templateFile: 'plop-templates/backend/hardhat.config.hbs'
+                templateFile: 'plop-templates/backend/hardhat.config.hbs',
+                data:{
+                    network: NETWORK
+                }
             },
             {
                 type: 'add',
-                path: `../${BACKEND_DIR_NAME}/contracts/{{pascalCase contractName}}.sol`,
-                templateFile: 'plop-templates/backend/contract.hbs'
+                path: `../${BACKEND_DIR_NAME}/contracts/${toPascalCase(CONTRACT_NAME)}.sol`,
+                templateFile: 'plop-templates/backend/contract.hbs',
+                data:{
+                    contractName: CONTRACT_NAME
+                }
             },
             {
                 type: 'add',
                 path: `../${BACKEND_DIR_NAME}/scripts/deploy.js`,
-                templateFile: 'plop-templates/backend/deploy.hbs'
+                templateFile: 'plop-templates/backend/deploy.hbs',
+                data:{
+                    contractName: CONTRACT_NAME
+                }
             },
         ]
     });
 
     plop.setGenerator('frontend-generator', {
         description: "installing and generating frontend dependencies and file",
-        prompts: [
-            {
-                type: 'input',
-                name: 'contractName',
-                message: 'Press enter!, for contract name',
-                default: CONTRACT_NAME
-            },
-            {
-                type: 'input',
-                name: 'network',
-                message: 'Press enter!, for network',
-                default: NETWORK
-            }
-        ],
+        prompts: [],
         actions: [
             {
                 type: 'add',
                 path: `../${FRONTEND_DIR_NAME}/constants/index.js`,
-                templateFile: 'plop-templates/frontend/constant.hbs'
+                templateFile: 'plop-templates/frontend/constant.hbs',
+                data:{
+                    contractName: CONTRACT_NAME
+                }
             },
             {
                 type: 'add',
                 path: `../${FRONTEND_DIR_NAME}/pages/index.js`,
-                templateFile: 'plop-templates/frontend/index.hbs'
+                templateFile: 'plop-templates/frontend/index.hbs',
+                data:{
+                    contractName: CONTRACT_NAME,
+                    network: NETWORK
+                }
             },
         ]
     })
